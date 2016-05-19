da61ee1940cf2d6eaf08ae034002d394b689e9e7
Lukasz Oleksak <l.oleksak@samsung.com>
2016-05-19 14:18:41 +0200

    * added dbus proxy calls for ReadCommand method in org.tizen.DirectReading interface exposed on A11 Bus * added mechanism for calling reading stop/cancel smart calbacks on Evas_Object in response to incomming signals from org.tizen.DirectReading interface
    Change-Id: I354d75dd3e23c5725c6def508d7639be7c4852bd

--- atspi_output/src/mod.c
+++ atspi_output/src/mod.c
@@ -28,24 +28,69 @@
 #define PATH "/org/tizen/DirectReading"
 #define INTERFACE "org.tizen.DirectReading"
 
+static const char SIG_READ_STOP[] = "access,read,stop";
+static const char SIG_READ_CANCEL[] = "access,read,cancel";
+
 static void (*cb_func) (void *data);
 static void *cb_data;
 
 static Eldbus_Connection *conn = NULL;
 static Eldbus_Object *dobj = NULL;
 static Eldbus_Proxy *proxy = NULL;
+static Eina_Hash *evas_obj_by_read_command_id = NULL;
 
 static void
-_on_send_text(void *data EINA_UNUSED, const Eldbus_Message *msg, Eldbus_Pending *pending EINA_UNUSED)
+_on_proxy_call(void *data, const Eldbus_Message *msg, Eldbus_Pending *pending EINA_UNUSED)
 {
    const char *errname, *errmsg;
-   char *s;
+   const char *s;
+   const Eina_Bool b;
+   const int32_t i;
+   Evas_Object *obj = data;
 
-   SLOG(LOG_DEBUG, LOG_TAG, "[START] ?");
+   SLOG(LOG_DEBUG, LOG_TAG, "[START] processing proxy call reply");
+
    if (eldbus_message_error_get(msg, &errname, &errmsg))
      {
         SLOG(LOG_ERROR, LOG_TAG, "%s %s", errname, errmsg);
         return;
+     }
+
+   if (obj)
+     {
+        // get read command id and mapp it to obj
+        if (eldbus_message_arguments_get(msg, "sbi", &s,&b,&i))
+          {
+             eina_hash_add(evas_obj_by_read_command_id,&i,obj);
+          }
+     }
+}
+
+static void
+_on_reading_stopped(void *context EINA_UNUSED, const Eldbus_Message *msg)
+{
+   const int32_t i;
+   if (eldbus_message_arguments_get(msg, "i", &i))
+     {
+        Evas_Object *obj = eina_hash_find(evas_obj_by_read_command_id,&i);
+        if (obj)
+          {
+             evas_object_smart_callback_call(obj, SIG_READ_STOP, NULL);
+          }
+     }
+}
+
+static void
+_on_reading_cancelled(void *context EINA_UNUSED, const Eldbus_Message *msg)
+{
+   const int32_t i;
+   if (eldbus_message_arguments_get(msg, "i", &i))
+     {
+        Evas_Object *obj = eina_hash_find(evas_obj_by_read_command_id,&i);
+        if (obj)
+          {
+             evas_object_smart_callback_call(obj, SIG_READ_CANCEL, NULL);
+          }
      }
 }
 
@@ -90,6 +135,10 @@ _get_a11y_address(void *data, const Eldbus_Message * msg, Eldbus_Pending * pendi
      }
 
    SLOG(LOG_DEBUG, LOG_TAG, "Proxy object is created");
+
+   eldbus_proxy_signal_handler_add(proxy, "ReadingStopped", _on_reading_stopped, NULL);
+   eldbus_proxy_signal_handler_add(proxy, "ReadingCancelled", _on_reading_cancelled, NULL);
+
  end:
    eldbus_connection_unref(session);
    SLOG(LOG_DEBUG, LOG_TAG, "[END]");
@@ -103,6 +152,7 @@ elm_modapi_init(void *m )
    Eldbus_Connection *session;
    Eldbus_Message *msg;
 
+   evas_obj_by_read_command_id = eina_hash_int32_new(NULL);
    eldbus_init();
    session = eldbus_connection_get(ELDBUS_CONNECTION_TYPE_SESSION);
    if (!session)
@@ -149,20 +199,36 @@ elm_modapi_shutdown(void *m )
 }
 
 EAPI void
-out_read(const char *txt)
+out_read(Evas_Object *obj, const char *txt)
 {
    int ret = 0;
-   SLOG(LOG_DEBUG, LOG_TAG, "[START] sending text (txt: %s)", txt);
+   SLOG(LOG_DEBUG, LOG_TAG, "[START] requesting interruptible reading (txt: %s)", txt);
+   if (!proxy)
+     {
+        SLOG(LOG_ERROR, LOG_TAG, "Proxy object is NULL!");
+        return;
+     }
+   //TODO: remove "Hello" call after dbus communication is verified
+   eldbus_proxy_call(proxy, "Hello", _on_proxy_call, NULL, -1, "s", txt);
+   eldbus_proxy_call(proxy, "ReadCommand", _on_proxy_call, obj, -1, "sb", txt, EINA_TRUE);
+   return;
+}
+
+
+EAPI void
+out_force_read(Evas_Object *obj, const char *txt)
+{
+   int ret = 0;
+   SLOG(LOG_DEBUG, LOG_TAG, "[START] requesting uninterruptible reading (txt: %s)", txt);
    if (!proxy)
      {
         SLOG(LOG_ERROR, LOG_TAG, "Proxy object is NULL!");
         return;
      }
 
-   eldbus_proxy_call(proxy, "Hello", _on_send_text, NULL, -1, "s", txt);
+   eldbus_proxy_call(proxy, "ReadCommand", _on_proxy_call, obj, -1, "sb", txt, EINA_FALSE);
    return;
 }
-
 
 EAPI void
 out_cancel(void)
